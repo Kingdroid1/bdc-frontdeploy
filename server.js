4abc6849814c6bd79dd491648d48b392a2e704b6
feyex <feyisewaawotedu@gmail.com>
2019-08-08 13:09:12 +0100

    dep

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
 
 app.use(cors());
+app.use(history());
 app.use(serveStatic('./dist'));
 
 // app.get('/*', function(req,res) {
