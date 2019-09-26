4a992a948a2f39394f3ebe546f4873ffd9b41e4e
nkingdroid <nking@sbsc.com>
2019-09-26 11:38:56 +0100

    Replacing URLs ---

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1017,7 +1017,7 @@ export default {
         },
 
         data: {
-          csvURL: `http://localhost:5000/api/rates/csv?location=${this.selectedLocation}&currency=${this.baseCurrency}`,
+          csvURL: `https://naija-bdc.herokuapp.com/api/rates/csv?location=${this.selectedLocation}&currency=${this.baseCurrency}`,
           beforeParse: function(csv) {
             return csv.replace(/\n\n/g, "\n");
           }
@@ -1165,7 +1165,7 @@ export default {
       
       email= this.sub.email;
         
-      const API_URL = "http://localhost:5000/api/suscribe/"+email;
+      const API_URL = "https://naija-bdc.herokuapp.com/api/suscribe/"+email;
         
       await axios
         .get(API_URL,email)
@@ -1220,7 +1220,7 @@ export default {
 
     //fetch news to display
     async getNews() {
-      const API_URL = "http://localhost:5000/api/news";
+      const API_URL = "https://naija-bdc.herokuapp.com/api/news";
 
       await axios
         .get(API_URL)
@@ -1369,7 +1369,7 @@ export default {
         return false;
       });
     });
-    const API_URL = "http://localhost:5000/api/operators/operators";
+    const API_URL = "https://naija-bdc.herokuapp.com/api/operators/operators";
     axios
       .get(API_URL)
       .then(response => {
